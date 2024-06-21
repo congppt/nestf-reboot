de7f24ecb9f67b97e44377a3dc57008743ea1a10
congppt <congppt2002@gmail.com>
2024-06-21 21:42:30 +0700

    fix: migrate db

---
+++ NestF.Infrastructure/Migrations/20240621143909_AddProductStatus.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NestF.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMale",
+                table: "Accounts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsMale",
+                table: "Accounts");
+        }
+    }
+}
