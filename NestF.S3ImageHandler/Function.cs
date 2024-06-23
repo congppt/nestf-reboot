05478fc4e4e5aa5ca93fc58ff3949d1853815624
congppt <congppt2002@gmail.com>
2024-06-23 11:14:09 +0700

    fix: image webhook

--- NestF.S3ImageHandler/Function.cs
+++ NestF.S3ImageHandler/Function.cs
@@ -15,7 +15,8 @@ namespace NestF.S3ImageHandler;
 public class Function
 {
     IAmazonS3 S3Client { get; set; }
-    
+    public const string PRODUCT_IMG_FOLDER = "product-images";
+    public const string HOST = "https://nest-f-f8fbc9gbf2g5dteu.southeastasia-01.azurewebsites.net/";
     public Function()
     {
         S3Client = new AmazonS3Client(RegionEndpoint.APSoutheast1);
@@ -42,7 +43,7 @@ public class Function
                 var response = await S3Client.GetObjectMetadataAsync(s3Event.Bucket.Name, s3Event.Object.Key);
                 context.Logger.LogInformation(response.Headers.ContentType);
                 context.Logger.LogInformation(s3Event.Object.Key + "hit lambda");
-                await SendWebhookEvent(s3Event.Object.Key);
+                await SendWebhookEvent(s3Event.Object.Key, context);
             }
             catch (Exception e)
             {
@@ -55,29 +56,24 @@ public class Function
         }
     }
 
-    public async Task SendWebhookEvent(string imagePath)
+    public async Task SendWebhookEvent(string imagePath, ILambdaContext context)
     {
         var parameters = imagePath.Split(['/', '_']);
-        var uri = Constants.HOST;
+        var uri = HOST;
         var id = int.Parse(parameters[1]);
         switch (parameters[0])
         {
-            case Constants.PRODUCT_IMG_FOLDER:
-                uri += $"api/products/{id}";
+            case PRODUCT_IMG_FOLDER:
+                uri += $"products/{id}";
                 break;
         }
-        var payload = JsonSerializer.Serialize(new { imagePath });
+        string secret = string.Empty;
+        context.Logger.LogInformation($"Uri: {uri}");
+        var payload = JsonSerializer.Serialize(new { imagePath, secret });
         var client = new HttpClient();
-        client.BaseAddress = new Uri(Constants.HOST);
         //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "");
         var content = new StringContent(payload, Encoding.UTF8, "application/json");
         var response = await client.PatchAsync(uri, content);
         response.EnsureSuccessStatusCode();
     }
-}
-
-class Constants
-{
-    public const string PRODUCT_IMG_FOLDER = "product-images";
-    public const string HOST = "https://nest-f-f8fbc9gbf2g5dteu.southeastasia-01.azurewebsites.net/";
 }
